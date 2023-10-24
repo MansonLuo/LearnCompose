101e4a7f2326b83bf750b2a5473d161d7cf46904
MansonLuo <1558911620@qq.com>
2023-10-24 17:47:33 +0800

    saved.

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,7 +17,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.learncompose.ui.theme.LearnComposeTheme
@@ -42,105 +40,61 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
-    Content()
+    Factorial()
 }
 
+@Preview
 @Composable
-fun ListOfChoices(
-    onNumberSelect: (number: Int) -> Unit
-) {
-    Column(
+fun Factorial() {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+    val text = remember {
+        mutableStateOf(
+            factorialAsString(0)
+        )
+    }
+
+    Box(
         modifier = Modifier
-            .fillMaxHeight()
+            .fillMaxSize()
             .padding(8.dp),
-        verticalArrangement = Arrangement.Center
+        contentAlignment = Alignment.Center
     ) {
-        (0 until 10).forEach { number ->
-            Button(
-                onClick = {
-                    onNumberSelect(number)
-                }
-            ) {
-                Text(
-                    text = stringResource(R.string.number_select_pattern, number),
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
+        Text(
+            text = text.value,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.clickable {
+                expanded.value = true
+            }
+        )
+
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false }
+        ) {
+            (0 until 10).forEach {
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "$it!")
+                    },
+                    onClick = {
+                        text.value = factorialAsString(it)
+                        expanded.value = false
+                    }
                 )
             }
         }
     }
 }
 
-@Preview(
-    name = "number list"
-)
-@Composable
-fun Preview_ListOfChoices() {
-    ListOfChoices(
-        onNumberSelect = {}
-    )
-}
-
-@Composable
-fun ComposeResult(
-    numberSelected: Int,
-    res: Int
-) {
-    Text(
-        text = stringResource(R.string.number_result_pattern, numberSelected, res),
-        style = MaterialTheme.typography.bodyLarge
-    )
-}
-
-@Preview(
-    name = "compose result"
-)
-@Composable
-fun Preview_ComposeResult() {
-    ComposeResult(
-        numberSelected = 1,
-        res = 1
-    )
-}
-
-@Preview
-@Composable
-fun Content() {
-    val selectedNumber = remember {
-        mutableStateOf<Int>(0)
-    }
-    val composeResult = remember {
-        mutableStateOf<Int>(0)
-    }
-
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        ListOfChoices { number ->
-            selectedNumber.value = number
-            composeResult.value = getFactorialResult(selectedNumber.value)
-        }
-
-        ComposeResult(
-            numberSelected = selectedNumber.value,
-            res = composeResult.value
-        )
-    }
-}
-
-fun getFactorialResult(number: Int): Int {
-    if (number == 0) return 1
+fun factorialAsString(number: Int): String {
+    if (0 == number) return "0! = 0"
 
     var res = 1
-
-    (1 until number + 1).forEach {
-        res *= it
+    (1 until number + 1).forEach { n ->
+        res *= n
     }
 
-    return res
+    return "$number! = $res"
 }
