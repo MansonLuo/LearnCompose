8fe81481989dff155ea3498491afd8dd10d1ae6e
MansonLuo <1558911620@qq.com>
2023-10-26 13:28:29 +0800

    finished PredefinedLayoutDemo.

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -3,18 +3,28 @@ package com.example.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.DrawModifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,39 +48,125 @@ class MainActivity : ComponentActivity() {
 fun App() {
 }
 
-@Preview(
-    widthDp = 400,
-    heightDp = 300
-)
+@Preview
 @Composable
-fun MyCustomText() {
-    Text(
-        text = "Hello Compose",
-        textAlign = TextAlign.Center,
-        style = MaterialTheme.typography.headlineLarge,
+fun PredefinedLayoutDemo() {
+    var isRedChecked by remember {
+        mutableStateOf(true)
+    }
+    var isGreenChecked by remember {
+        mutableStateOf(true)
+    }
+    var isBlueChecked by remember {
+        mutableStateOf(true)
+    }
+
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .drawYellowCross()
+            .padding(16.dp)
+    ) {
+        CheckBoxWithTag(checked = isRedChecked, text = "Red") {
+            isRedChecked = it
+        }
+        CheckBoxWithTag(checked = isGreenChecked, text = "Green") {
+            isGreenChecked = it
+        }
+        CheckBoxWithTag(checked = isBlueChecked, text = "Blue") {
+            isBlueChecked = it
+        }
+
+
+        StackedBoxes(
+            isOuterBoxShow = isRedChecked,
+            isMiddleBoxShow = isGreenChecked,
+            isInnerBoxShow = isBlueChecked
+        )
+    }
+}
+
+@Composable
+fun CheckBoxWithTag(
+    checked: Boolean,
+    text: String,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable {
+                onCheckedChange(!checked)
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            modifier = Modifier.alignByBaseline()
+        )
+
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .alignByBaseline()
+                .padding(start = 8.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun Preview_CheckBoxWithTag() {
+    CheckBoxWithTag(
+        checked = true,
+        text = "Red",
+        onCheckedChange = {}
     )
 }
 
-fun Modifier.drawYellowCross() = then(
-    object : DrawModifier {
-        override fun ContentDrawScope.draw() {
-            drawLine(
-                Color.Yellow,
-                start = Offset(0f, 0f),
-                end = Offset(size.width - 1, size.height - 1),
-                strokeWidth = 10f
-            )
-            drawLine(
-                color = Color.Yellow,
-                start = Offset(0f, size.height - 1),
-                end = Offset(size.width - 1, 0f),
-                strokeWidth = 10f
-            )
+@Composable
+fun StackedBoxes(
+    isOuterBoxShow: Boolean,
+    isMiddleBoxShow: Boolean,
+    isInnerBoxShow: Boolean
+) {
 
-            drawContent()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp)
+    ) {
+        if (isOuterBoxShow) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Red)
+            )
+        }
+
+        if (isMiddleBoxShow) {
+            Box(
+                modifier = Modifier.fillMaxSize()
+                    .padding(32.dp)
+                    .background(Color.Green)
+            )
+        }
+
+        if (isInnerBoxShow) {
+            Box(
+                modifier = Modifier.fillMaxSize()
+                    .padding(64.dp)
+                    .background(Color.Blue)
+            )
         }
     }
-)
+}
+
+@Preview
+@Composable
+fun Preview_StackedBoxes() {
+    StackedBoxes(isOuterBoxShow = true, isMiddleBoxShow = true, isInnerBoxShow = true)
+}
