c8e51695e610160dd17af6b3bd2e69dd7f3489fe
MansonLuo <1558911620@qq.com>
2023-10-27 21:00:00 +0800

    saved

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,9 +20,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learncompose.ui.theme.LearnComposeTheme
-import java.util.Date
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,31 +44,90 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
+    RadioButtonGroup()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TemperaturePanel(
+    temperature: Float,
+    onTemperatureChange: (String) -> Unit,
+    unit: TemperatureUnit,
+    onUnitChange: () -> Unit
+) {
+    Column {
+        TextField(
+            value = temperature.toString(),
+            onValueChange = onTemperatureChange
+        )
+    }
+}
+
+
+@Composable
+fun RadioButtonGroup(
+    options: List<String>,
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit
+) {
+    Column {
+        options.forEach { label ->
+            RadioButtonWithLabel(
+                option = label,
+                selected = selectedOption == label
+            ) {
+                onOptionSelected(label)
+            }
+        }
+    }
 }
 
 @Preview
 @Composable
-fun RememberWithKeyDemo() {
-
-    var key by remember {
-        mutableStateOf(false)
-    }
-    var date by remember(key) {
-        mutableStateOf(Date())
+fun RadioButtonGroup() {
+    val options = listOf<String>("华氏", "摄氏")
+    var selectedOption by remember {
+        mutableStateOf(options[0])
     }
 
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    RadioButtonGroup(
+        options = options,
+        selectedOption = selectedOption
+    ) { selectedLabel ->
+        selectedOption = selectedLabel
+    }
+}
+
+@Composable
+fun RadioButtonWithLabel(
+    option: String,
+    selected: Boolean,
+    onOptionSelected: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = date.toString())
-        Button(
-            onClick = {
-                key = !key
-            }
-        ) {
-            Text(text = "更新")
+        RadioButton(selected = selected, onClick = onOptionSelected)
+        Text(
+            text = option,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(
+                end = 16.dp
+            )
+        )
+    }
+}
+
+sealed class TemperatureUnit {
+    object HuaShi: TemperatureUnit() {
+        override fun toString(): String {
+            return "华氏"
+        }
+    }
+
+    object SheShi: TemperatureUnit() {
+        override fun toString(): String {
+            return "摄氏"
         }
     }
 }
